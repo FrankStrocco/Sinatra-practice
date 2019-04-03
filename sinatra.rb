03d0caba14fc998ce32658aa5b27f63f22431d20
Frank Strocco <frank.strocco@gmail.com>
2019-04-02 21:48:34 -0700

    finished up this part of the sinatra tutorial

--- sinatra.rb
+++ sinatra.rb
@@ -1,19 +1,57 @@
 require 'sinatra'
+
     get '/' do
-        filepath = File.join(settings.public_folder, 'index.html')
-        File.read(filepath)
+        @page_title = "welcome to Explore Cali, YO!!!"
+        @body_id = "home"
+        erb :index
     end
 
-    # get '/company/*' do
-    #     #all splats stored in an array
-    #     subject = params[:splat].first
-    #     "You requested: #{subject}"
-    # end
-    #
-    # get '/company/:subject' do
-    #     "You requested #{params[:subject]}"
-    # end
-    #
-    # get '/product/:id' do
-    #     "Your product id: #{params[:id]}"
-    # end
+    get '/tours' do
+        @page_title = "Tour Page, YO!"
+        erb :tours
+    end
+
+    get '/support' do
+        @page_title = "Support Page, YO!"
+        erb :support
+    end
+
+    get '/contact' do
+        @page_title = "Contact Page, YO!"
+        erb :contact
+    end
+
+    get '/mission' do
+        @page_title = "Contact Page, YO!"
+        erb :mission
+    end
+
+    get '/resources' do
+        @page_title = "Contact Page, YO!"
+        erb :resources
+    end
+
+    get '/explorers' do
+        @page_title = "Contact Page, YO!"
+        erb :explorers
+    end
+
+    get '/resources' do
+        @page_title = "Resources Page, YO!"
+        erb :resources
+    end
+
+    get '/resources/faq' do
+        @page_title = "Resources FAQ Page, YO!"
+        erb :'resources/faq'
+    end
+
+    get '/tours/tour_detail_backpack' do
+        @page_title = "tour detail backpack, YO!"
+        erb 'tours/tour_detail_backpack'.to_sym
+    end
+
+    get '/tours/tour_detail_bigsur' do
+        @page_title = "tour detail Birsur, YO!"
+        erb :'tours/tour_detail_bigsur'
+    end
